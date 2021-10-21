cb4fccd77aa91c2fd791e8d9a6d8fbb95617870d
matthewngid20 <matthew.ng.id20@gmail.com>
2021-10-21 11:18:37 +1100

    created views and layouts

--- App.js
+++ App.js
@@ -1,12 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Button, StyleSheet, Text, View, TouchableOpacity, TextInput } from 'react-native';
+import Constants from 'expo-constants'
 
 export default function App() {
+
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <View>
+        <Input
+          placeholder='Your task' />
+        <TouchableOpacity>
+          <Text>Add to list</Text>
+        </TouchableOpacity>
+      </View>
+      <View style={styles.header}>
+
+        <Text style={styles.task}> Testing</Text>
+
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.task}> Testing</Text>
+      </View>
+      <View style={styles.header}>
+        <Text style={styles.task}> Testing</Text>
+      </View>
+
     </View>
   );
 }
@@ -14,8 +35,22 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: 'powderblue',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginTop: Constants.statusBarHeight,
+  },
+  header: {
+    display: 'flex',
+    flexDirection: 'row',
+  },
+  task: {
+    backgroundColor: '#FFFFFF',
+    fontSize: 20,
+    borderColor: '#DDDDDD',
+    borderWidth: 1,
+    padding: 5,
+    flex: 1,
+
+
   },
 });
